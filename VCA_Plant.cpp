b3e5056ae6562e6cddc15adafa4d63e63649836b
hankyang94 <hankyang94@gmail.com>
2018-07-24 22:22:19 -0400

    plant now includes two motors

--- VCA_Plant.cpp
+++ VCA_Plant.cpp
@@ -8,28 +8,41 @@
 
 
 VCA_Plant::VCA_Plant(void)
-        : VCA_Plant(35,     // pin 35 for pwm A 1
+        : VCA_Plant(35,     // pin 35 for pwm A 1, PWM pin
                           36,     // pin 36 for pwm A 2, direction pin
-                          1.0,    // default amplitude +- 1 mm
-                          20,     // default frequency 20Hz
-                          33,     // pin 33 for reading hall effect sensor and AD620
+                          37,     // pin 37 for PWM B 1, PWM
+                          38,     // pin 38 for PWM B 2, DIR
+                          1.0,    // default amplitude +- 1 mm, for A
+                          20,     // default frequency 20Hz, for A
+                          1.0,    // default amplitude, for B
+                          20,     // default frequency, for B
+                          33,     // pin 33 for reading hall effect sensor and AD620, for A
+                          34,     // pin 34 for reading hall effect sensor and AD620, for B
                           7.6294e-5,  // resolution is 5V / 2^16, using 16 bit ADC
                           22      // takes about 22 micros to drive and read, print in console
                             ){}
 
-VCA_Plant::VCA_Plant(int motor_A_pwm_1, int motor_A_pwm_2, float motor_A_amplitude, int motor_A_frequency,
-                                 int motor_A_hall_pin, const float voltage_per_bit, const int drive_read_micros) :
+VCA_Plant::VCA_Plant(int motor_A_pwm_1, int motor_A_pwm_2, int motor_B_pwm_1, int motor_B_pwm_2,
+                     float motor_A_amplitude, int motor_A_frequency, float motor_B_amplitude, int motor_B_frequency,
+                                 int motor_A_hall_pin, int motor_B_hall_pin, const float voltage_per_bit, const int drive_read_micros) :
                                   _motor_A_pwm_1(motor_A_pwm_1),
                                   _motor_A_pwm_2(motor_A_pwm_2),
+                                  _motor_B_pwm_1(motor_B_pwm_1),
+                                  _motor_B_pwm_2(motor_B_pwm_2),
                                   _motor_A_amplitude(motor_A_amplitude),
                                   _motor_A_frequency(motor_A_frequency),
+                                  _motor_B_amplitude(motor_B_amplitude),
+                                  _motor_B_frequency(motor_B_frequency),
                                   _motor_A_hall_pin(motor_A_hall_pin),
+                                  _motor_B_hall_pin(motor_B_hall_pin),
                                   _voltage_per_bit(voltage_per_bit),
                                   _drive_read_micros(drive_read_micros){
     analogReadResolution(16);
     // change the default PWM frequency to eliminate PWM switching noise
     analogWriteFrequency(motor_A_pwm_1, 30000);
     analogWriteFrequency(motor_A_pwm_2, 30000);
+    analogWriteFrequency(motor_B_pwm_1, 30000);
+    analogWriteFrequency(motor_B_pwm_2, 30000);
 //    pinMode(motor_A_pwm_1, OUTPUT);
 //    pinMode(motor_A_pwm_2, OUTPUT);
 }
@@ -72,9 +85,32 @@ void VCA_Plant::DriveMotorADuty(float duty) {
     Serial1.println(duty_bit);
 }
 
+void VCA_Plant::DriveMotorBDuty(float duty) {
+    int duty_bit = int(duty*256);
+    //////////////// use one-channel MAX14870 motor driver //////////////////////
+    if (duty > 0) { // forward motor motion, PIN 1 PWM, PIN 2 HIGH
+        analogWrite(_motor_B_pwm_1, duty_bit);
+        digitalWrite(_motor_B_pwm_2, HIGH);
+    } else if (duty < 0) { // reverse motor motion, PIN 1 PWM, PIN 2 LOW
+        analogWrite(_motor_B_pwm_1, -duty_bit);
+        digitalWrite(_motor_B_pwm_2, LOW);
+    } else {}
+    Serial1.println(duty_bit);
+}
+
 void VCA_Plant::StopMotorA() {  // stop motor, pin 1 and pin 2 both low
     analogWrite(_motor_A_pwm_1, 0);
     analogWrite(_motor_A_pwm_2, 0);
+}
+
+void VCA_Plant::StopMotorB() {  // stop motor, pin 1 and pin 2 both low
+    analogWrite(_motor_B_pwm_1, 0);
+    analogWrite(_motor_B_pwm_2, 0);
+}
+
+void VCA_Plant::StopMotor() {  // stop motor, pin 1 and pin 2 both low
+    this->StopMotorA();
+    this->StopMotorB();
 }
 
 int VCA_Plant::ReadMotorAPositionBit() {
@@ -117,6 +153,50 @@ float VCA_Plant::MotorACalibration(int bit) {   // hall-effect sensor calibratio
             + a4*cos(4.0*bitF*w) + b4*sin(4.0*bitF*w)
             + a5*cos(5.0*bitF*w) + b5*sin(5.0*bitF*w)
             + a6*cos(6.0*bitF*w) + b6*sin(6.0*bitF*w);
+    ///////////////////////////////////////////
+}
+
+
+int VCA_Plant::ReadMotorBPositionBit() {
+    int position = analogRead(_motor_B_hall_pin);
+    Serial1.println(position);
+    return position;
+}
+
+float VCA_Plant::ReadMotorBPositionVoltage() {
+    int bit = this->ReadMotorBPositionBit();
+    return bit * _voltage_per_bit;
+}
+
+float VCA_Plant::ReadMotorBPositionMM() {
+    int bit = this->ReadMotorBPositionBit();
+    return this->MotorBCalibration(bit);   // convert to mm by using calibration results
+}
+
+float VCA_Plant::MotorBCalibration(int bit) {   // hall-effect sensor calibration results
+    float bitF = (float) bit;
+//    Serial.println(bitF);
+    //////////// use fourier 6, slower //////////////
+    float w = 9.745973193663611e-05;
+    float a0 = 2.677556372177879;
+    float a1 = 0.087802552266365;
+    float b1 = 2.132922208355770;
+    float a2 = -0.379712039244581;
+    float b2 = 1.173208902204697;
+    float a3 = -0.357648678874241;
+    float b3 = 0.611855241307163;
+    float a4 = -0.301231731752640;
+    float b4 = 0.228443777988462;
+    float a5 = -0.189415796590939;
+    float b5 = 0.049054710883965;
+    float a6 = -0.084774303948667;
+    float b6 = 0.009042376872602;
+    return a0 + a1*cos(bitF*w) + b1*sin(bitF*w)
+           + a2*cos(2.0*bitF*w) + b2*sin(2.0*bitF*w)
+           + a3*cos(3.0*bitF*w) + b3*sin(3.0*bitF*w)
+           + a4*cos(4.0*bitF*w) + b4*sin(4.0*bitF*w)
+           + a5*cos(5.0*bitF*w) + b5*sin(5.0*bitF*w)
+           + a6*cos(6.0*bitF*w) + b6*sin(6.0*bitF*w);
     ///////////////////////////////////////////
 }
 
--- VCA_Plant.h
+++ VCA_Plant.h
@@ -11,14 +11,21 @@ class VCA_Plant {
 public:
     VCA_Plant(void);     // default constructor
 
-    VCA_Plant(int motor_A_pwm_1, int motor_A_pwm_2, float motor_A_amplitude, int motor_A_frequency,
-                    int motor_A_hall_pin, const float voltage_per_bit, const int drive_read_micros);    // constructor with parameters
+    VCA_Plant(int motor_A_pwm_1, int motor_A_pwm_2, int motor_B_pwm_1, int motor_B_pwm_2,
+              float motor_A_amplitude, int motor_A_frequency, float motor_B_amplitude, int motor_B_frequency,
+              int motor_A_hall_pin, int motor_B_hall_pin, const float voltage_per_bit, const int drive_read_micros);    // constructor with parameters
 
     ~VCA_Plant(void);    // destructor
 
     void DriveMotorADuty(float duty);  // drive motor A with a duty cycle
 
+    void DriveMotorBDuty(float duty);  // drive motor B with a duty cycle
+
     void StopMotorA(void);  // stop sending pwm, stop the motor
+
+    void StopMotorB(void);  // stop sending PWM, stop the motor
+
+    void StopMotor(void);  // stop both motors
 
     int ReadMotorAPositionBit();     // read the senor data in raw bits
 
@@ -28,6 +35,15 @@ public:
 
     float MotorACalibration(int bit);  // calibration result for motor A
 
+    int ReadMotorBPositionBit();     // read the senor data in raw bits
+
+    float ReadMotorBPositionVoltage();  // read the sensor data in voltage
+
+    float ReadMotorBPositionMM();    // read the sensor data in mm
+
+    float MotorBCalibration(int bit);   // calibration result for motor B
+
+
     void DriveMotorASin(float motor_A_max_duty,  // maximum duty cycle
                         int motor_A_frequency,   // vibration frequency
                         int num_cycles,          // vibration cycles
@@ -36,12 +52,17 @@ public:
 
 
 private:
-    int _motor_A_pwm_1;           // Arduino pin number for AIN1
-    int _motor_A_pwm_2;           // Arduino pin number for AIN2
+    int _motor_A_pwm_1;           // Arduino pin number for A PWM
+    int _motor_A_pwm_2;           // Arduino pin number for A DIR
+    int _motor_B_pwm_1;           // Arduino pin number for B PWM
+    int _motor_B_pwm_2;           // Arduino pin number for B DIR
     float _motor_A_amplitude;       // motor A vibration amplitude
     int _motor_A_frequency;          // motor A vibration frequency
+    float _motor_B_amplitude;        // motor B vibration amplitude, in mm
+    int _motor_B_frequency;          // motor B vibration frequency, in HZ
 //    int _motor_A_position_bit;       // motor A position feedback from hall effect sensor+AD620, raw bits
     int _motor_A_hall_pin;             // Arduino pin number for motor A hall effect sensor
+    int _motor_B_hall_pin;             // Arduino pin number for motor B hall effect sensor
     const float _voltage_per_bit;    // scale for converting bits to voltage
 //    double _motor_A_position_voltage;   // raw bits converted to voltage
     const int _drive_read_micros;          // Teensy drive and read microseconds in total
